d37613107426d6a7be1614c3728f8fef92fe3729
Asep Ama Rosmana <asepamarosmana@gmail.com>
2026-01-05 19:41:06 +0700

    use env for database connection

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_widyatamakampuskos",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 con.connect((err) => {
   if (err) {
-    console.log("database tidak terkoneksi");
+    console.log('database tidak terkoneksi');
   } else {
-    console.log("database terkoneksi");
+    console.log('database terkoneksi');
   }
 });
 
