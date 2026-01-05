973f1b1436d01776341d27a6934323ef73617ccf
Asep Ama Rosmana <asepamarosmana@gmail.com>
2026-01-06 06:30:56 +0700

    set api url 2

--- config/config.js
+++ config/config.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+const con = mysql.createConnection(process.env.DATABASE_URL);
 
 con.connect((err) => {
   if (err) {
